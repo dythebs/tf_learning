178e75ce0e04266513b67f50b152e453d6caa908
dythebs <244271172@qq.com>
2018-07-22 00:54:01 -0700

    rename dir update readme.md

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 take notes while learning tensorflow
 
 # Structure of the repository
-* `./minist_softmax` [handwriting recognition by softmax](http://www.tensorfly.cn/tfdoc/tutorials/mnist_beginners.html)
+* `./mnist_softmax` [handwriting recognition by softmax](http://www.tensorfly.cn/tfdoc/tutorials/mnist_beginners.html)
--- mnist_softmax/main.py
+++ mnist_softmax/main.py
@@ -8,9 +8,9 @@ W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x,W) + b)
 y_ = tf.placeholder(dtype=tf.float32,shape=[None,10])
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 #评估模型
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
 accuary = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 #初始化
